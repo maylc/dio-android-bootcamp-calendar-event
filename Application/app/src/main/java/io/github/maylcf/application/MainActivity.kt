c9355bea9c1d74b3b259727637e60590e5fdda10
Mayara Felix <maylc.dev@gmail.com>
2021-09-19 19:47:53 -0400

    launching calendar intent.

--- Application/app/src/main/java/io/github/maylcf/application/MainActivity.kt
+++ Application/app/src/main/java/io/github/maylcf/application/MainActivity.kt
@@ -1,7 +1,12 @@
 package io.github.maylcf.application
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.provider.CalendarContract
+import android.provider.CalendarContract.CONTENT_URI
+import android.provider.CalendarContract.Events.EVENT_LOCATION
+import android.provider.CalendarContract.Events.TITLE
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +16,14 @@ class MainActivity : AppCompatActivity() {
 
         val btnSetEvent = findViewById<AppCompatButton>(R.id.set_event_button)
         btnSetEvent.setOnClickListener {
+            val intent = Intent(Intent.ACTION_INSERT)
+                    .setData(CONTENT_URI)
+                    .putExtra(TITLE, "Bootcamp Android")
+                    .putExtra(EVENT_LOCATION, "Online")
+                    .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, System.currentTimeMillis())
+                    .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, System.currentTimeMillis() + (60*60*1000))
+
+            startActivity(intent)
         }
     }
 }
